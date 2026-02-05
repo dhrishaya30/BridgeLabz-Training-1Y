a09dcdd87f3ca29590933c8d5f8b74c82fbcab4f
dhrishaya30 <dhrishayak@gmail.com>
2026-02-05 15:00:01 +0530

    commit msg

---
+++ StringExmp.java
@@ -0,0 +1,30 @@
+ class StringExmp {
+ public static void main(String[] args){
+ 
+
+String s = "Drishya";
+//length()
+System.out.println(s.length());
+
+//toUpperCase()
+System.out.println(s.toUpperCase());
+ 
+ // toLowerCase()
+ System.out.println(s.toLowerCase());
+ 
+ //charAt()
+ System.out.println(s.charAt(5));
+ 
+ //substring()
+ System.out.println(s.substring(5));
+ System.out.println(s.substring(0 ,4));
+ 
+ //contains()
+ System.out.println(s.contains("java"));
+  
+ //equals()
+ System.out.println(s.equals("java programming"));
+ }
+ }
+ 
+ 
