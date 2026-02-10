7b4fb40078722f06e73481ecb867921c6be00710
dhrishaya30 <dhrishayak@gmail.com>
2026-02-10 22:37:07 +0530

    added solutions to given problems and removed garbage

--- ArrayMethodExample.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-class ArrayMethodExample{
-public static void main(String[] args){
-Scanner sc= new Scanner(System.in);
-int ar[]={10,20,30,40,50};
-Arrays.sort(ar);
-System.out.println(ar);
-}
-}
--- Whileloop.java
+++
@@ -1,9 +0,0 @@
-class Whileloop{
-public static void main (String[] args) {
-int i =1; // initialising variable
-while (i<=20) {
-System.out.println(i+" ");
-i++;
-}
-}
-}
